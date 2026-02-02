14f5971f872b5f2883570c3c042102db69f07a2f
Canberk Demir <65964608+CanberkDemir@users.noreply.github.com>
2026-02-02 13:29:12 +0300

    Worked on md initialization

--- src/mdsim/engine/init_sim.py
+++ src/mdsim/engine/init_sim.py
@@ -1,12 +1,27 @@
 from ase import Atoms
+from ase.calculators.lj import LennardJones
 from mdsim.io.read_config import load_config
+import numpy as np
 
-def create_atoms(config):
-    return None
+def createAseSystem(config):
+    system = Atoms(config['system']['atoms'], 
+                 cell=config['system']['box'], 
+                 pbc=[1, 1, 1]
+                 )
+    if 'lennard' or 'jones' in config['forces']['type']:
+        # You can also add cutoff here think about it
+        lj_instance = LennardJones(
+                sigma=config['forces']['sigma'],
+                epsilon=config['forces']['epsilon'],
+                )
+        system.calc = lj_instance
+    return system
 
-def initialize_md(Atoms, config):
+def initializeMd(system, config):
     return None
 
 def main():
     cfg = load_config('configs/lf_nvt.toml')
+    system = createAseSystem(cfg)
+    print(system)
     print(cfg)
